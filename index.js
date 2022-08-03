327984d7c68cf784b1d997caa8265daf03253a2e
rikiarizona <rikiarizona@gmail.com>
2022-08-03 10:01:35 +0700

    TUGAS 17-1 JAVACRIPT
    saya kirimkan tugas niomic

---
+++ index.js
@@ -0,0 +1,9 @@
+function MinMax() {
+    var katapendek = "Saya beLajar bahaSa peMrograman deNgan khUsuk.";
+    console.log(katapendek.toLowerCase());
+    console.log(katapendek.toUpperCase());
+    var katapanjang = "Allen SedAng MemAncinG IkaN di SuNgai, DIA MEndapat IkAN GaBuS 3 Ekor dan LanGsuNg Dijual.";
+    console.log(katapanjang.toLowerCase());
+    console.log(katapanjang.toUpperCase());
+}
+MinMax();
